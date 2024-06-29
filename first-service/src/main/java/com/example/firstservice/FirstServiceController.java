fc9d84f98c701ab8d88fa42fcf3791889ad56815
msk226 <kminseok5167@gmail.com>
2024-06-29 14:54:46 +0900

    FEAT: Spring Cloud gateway filter 적용 및 User MicroService 생성

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,10 +1,13 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
 public class FirstServiceController {
 
@@ -13,4 +16,14 @@ public class FirstServiceController {
         return "Welcome to the First service.";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, This is First service.";
+    }
 }
--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,10 +1,13 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/second-service")
 public class SecondServiceController {
 
@@ -13,4 +16,14 @@ public class SecondServiceController {
         return "Welcome to the Second service.";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, This is Second service.";
+    }
 }
