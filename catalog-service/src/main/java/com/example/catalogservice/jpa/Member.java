91ed8da712bacbdd2681f8b52143aaa5ea6323c5
msk226 <kminseok5167@gmail.com>
2024-06-30 14:53:29 +0900

    FEAT: CatalogService Entity, DTO, Return 객체 생성

--- catalog-service/src/main/java/com/example/catalogservice/jpa/Member.java
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/Member.java
@@ -1,11 +1,10 @@
-package com.example.catalogservice.entity;
+package com.example.catalogservice.jpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Generated;
 
 @Entity
 @Data
---
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,15 @@
+package com.example.catalogservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseCatalog {
+    private String productId;
+    private String productName;
+    private Integer stock;
+    private Integer unitPrice;
+    private Date createdAt;
+}
