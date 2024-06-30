634928ccc8efcf8335ea65eea71a326cfe528c2b
msk226 <kminseok5167@gmail.com>
2024-06-30 16:11:22 +0900

    FEAT: OrderService Order 생성 및 조회 API 구현

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,7 +1,21 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.dto.OrderDTO;
+import com.example.orderservice.jpa.OrderEntity;
 import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +24,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+    private final Environment env;
 
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in Order Service on Port %s",
+            env.getProperty("local.server.port"));
+    }
 
+    @PostMapping("/{userId}/orders")
+    public ResponseOrder createOrder(@PathVariable String userId, @RequestBody RequestOrder req){
+        OrderDTO orderDTO = new ModelMapper().map(req, OrderDTO.class);
+        orderDTO.setUserId(userId);
+        OrderDTO order = orderService.createOrder(orderDTO);
+        return new ModelMapper().map(order, ResponseOrder.class);
+    }
 
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<List<ResponseOrder>> getOrders(@PathVariable String userId){
+        Iterable<OrderEntity> orders = orderService.getOrdersByUserId(userId);
+        List<ResponseOrder> results = new ArrayList<>();
+        ModelMapper modelMapper = new ModelMapper();
+        orders.forEach(v -> {
+            results.add(modelMapper.map(v, ResponseOrder.class));
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(results);
+    }
 }
