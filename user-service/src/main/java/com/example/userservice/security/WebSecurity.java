ca861ea4260ce3d02b1d8b27d47cddf497c2f01f
msk226 <kminseok5167@gmail.com>
2024-07-10 15:59:47 +0900

    FEAT: DiscoveryService Dockerfile 생성

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -54,7 +54,7 @@ public class WebSecurity {
                     .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
 //                        .requestMatchers("/**").access(this::hasIpAddress)
                     .requestMatchers("/**").access(
-                        new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('192.168.219.235')"))
+                        new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('172.30.1.1')"))
                     .anyRequest().authenticated()
             )
             .authenticationManager(authenticationManager)
